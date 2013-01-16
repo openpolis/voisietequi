f430911f50fe2346c816ee64da43ecd4564fa5b4
verganis <stefano.vergani.it@gmail.com>
2013-01-16 10:15:34 +0100

    Commented URL for Questionario partiti before Early bird deployment

--- vsq/urls.py
+++ vsq/urls.py
@@ -9,9 +9,9 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', EarlyBirdView.as_view(), name='earlybird'),
     url(r'^registration_ok$',TemplateView.as_view(template_name='early_bird_success.html')),
-    url(r'^mockup_answer$', mockup_response),
-    url(r'^questionario/(?P<party_key>[-\w]+)/$', QuestionarioPartitiView.as_view(), name="questionario_partiti"),
-    url(r'^questionario/(?P<slug>[-\w]+)/completato$', QuestionarioPartitiClosed.as_view(), name="questionario_partiti_fine"),
+#    url(r'^mockup_answer$', mockup_response),
+#    url(r'^questionario/(?P<party_key>[-\w]+)/$', QuestionarioPartitiView.as_view(), name="questionario_partiti"),
+#    url(r'^questionario/(?P<slug>[-\w]+)/completato$', QuestionarioPartitiClosed.as_view(), name="questionario_partiti_fine"),
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
